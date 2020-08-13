9ae9f8ecc3a215c887e038312ddb2940b5681f56
Jenae Christensen <729jenae.christensen@gmail.com>
2020-08-12 23:32:36 -0400

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base 
     has_secure_password #alows us to authanticate 
+
+    validates :name, presence: true 
+    validates :email, presence: true
+    validates :email, uniqueness: true #no 2 users w the same email
+
     has_many :travel_destinations
 end 
