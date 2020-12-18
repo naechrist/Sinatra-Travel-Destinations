e8b88017fe1a900e56cd59434fe1dff85dbc790c
nae <nae@Jen-Christensens-iMac.local>
2020-12-18 14:49:23 -0500

    deleted seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-#Create some users 
-nae = User.create(name: "Nae", email: "nae@gmail.com", password:"password")
-jen = User.create(name: "Jen", email: "jen@gmail.com", password:"password")
+# #Create some users 
+# nae = User.create(name: "Nae", email: "nae@gmail.com", password:"password")
+# jen = User.create(name: "Jen", email: "jen@gmail.com", password:"password")
 
-#Create some destinations 
-TravelDestination.create(content: "Churchill Downs in Louisville, Kentucky", user_id: nae.id)
-TravelDestination.create(content: "Grand Canyon in Arizona", user_id: nae.id)
+# #Create some destinations 
+# TravelDestination.create(content: "Churchill Downs in Louisville, Kentucky", user_id: nae.id)
+# TravelDestination.create(content: "Grand Canyon in Arizona", user_id: nae.id)
 
